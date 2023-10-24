562b6d838a4468639c720869081d440cebe13f92
queen <Queenmoncler@gmail.com>
2023-10-24 19:16:40 +0300

    add

---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Collection<Post> all() {
+        return repository.all();
+
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
+
